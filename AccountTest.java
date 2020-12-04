9171d3da5e2ccb9032cd55c6b7a70428e98116b0
yana-kroasana <72204689+yana-kroasana@users.noreply.github.com>
2020-12-04 14:33:27 +0200

    Add files via upload

---
+++ AccountTest.java
@@ -0,0 +1,12 @@
+package staticmodifier;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account account = new Account(1222, 20000);
+		acoount.withdraw(2500);
+		account.deposit(3000);
+		System.out.println("Balance " + account.getBalance() + "\nMonthly interest" + account.getMonthlyInterest() + "\nDate created" + account.getDateCreated());
+	}
+}
